6ddf5649790ba60dd1e0ee59f92678560a5424db
nicholsx <nichols.costa@gmail.com>
2021-06-30 17:09:01 -0300

    Classe Empregado criada
    close #3

--- Atividade8/src/main/java/com/teste/Empregado.java
+++ Atividade8/src/main/java/com/teste/Empregado.java
@@ -3,10 +3,10 @@ package com.teste;
 /**
  * 
  * @author Nicholas
- * Integrando ao GitHub o projeto
+ * Integrando ao GitHub o projeto Atividade8
  */
 
-public class Empregado extends EmpregadoTerceirizado{
+public class Empregado{
 
 		public String nome;
 		public int horas;
@@ -16,66 +16,8 @@ public class Empregado extends EmpregadoTerceirizado{
 			super();
 			this.nome = nome;
 			this.horas = horas;
-			this.valorPorHora = valorPorHora;
-			this.adicional = adicional;
+			this.valorPorHora = valorPorHora;		
 		}
-
-		public double realizarPagamento() {
-			
-			double pagamento;
-			
-			pagamento = (valorPorHora * horas) + adicional;
-			
-			if (pagamento >= 1100) {
-				System.out.println("Valor do pagamento de " +nome+ ": " + pagamento);
-			}
-			else {
-				pagamento = 1100;
-				System.out.println("Valor do pagamento de " +nome+ ": " + pagamento);
-			}
-			
-			return pagamento;
-			
-		}
-
-		public String getNome() {
-			return nome;
-		}
-
-		public void setNome(String nome) {
-			this.nome = nome;
-		}
-
-		public int getHoras() {
-			System.out.println("Valor do pagamento de " +nome+ ": " + horas);
-			return horas;
-		}
-
-		public void setHoras(int horas) {
-			if (horas > 40) {
-				throw new IllegalArgumentException("Digite a quantidade de horas maior/igual a 0 e menor/igual a 40");
-			}
-			
-			this.horas = horas;
-				
-		}
-
-		public double getValorPorHora() {
-			return valorPorHora;
-		}
-
-		public void setValorPorHora(double valorPorHora) {
-			
-			if (valorPorHora < 30) {
-				throw new IllegalArgumentException("Valor minimo R$30,00 / Valor máximo R$200,00");	
-			}
-			else if (valorPorHora > 200) {
-				throw new IllegalArgumentException("Valor minimo R$30,00 / Valor máximo R$200,00");	
-			}
-			
-			this.valorPorHora = valorPorHora;	
-		}
-
 }
 
 	
