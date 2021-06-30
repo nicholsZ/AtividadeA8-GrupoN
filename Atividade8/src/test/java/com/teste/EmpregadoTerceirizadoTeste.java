df240c2db60349f15993d9b441a460e0a6d986e1
nicholsZ <86330478+nicholsZ@users.noreply.github.com>
2021-06-30 18:03:55 -0300

    Update EmpregadoTerceirizadoTeste.java

--- Atividade8/src/test/java/com/teste/EmpregadoTerceirizadoTeste.java
+++ Atividade8/src/test/java/com/teste/EmpregadoTerceirizadoTeste.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class EmpregadoTerceirizadoTeste {
+public class EmpregadoTerceirizadoTest {
 
 	@Test
 	public void testSetAdicional() {
