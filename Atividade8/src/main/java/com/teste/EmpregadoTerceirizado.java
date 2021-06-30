ceec27bf811522a015812f73acdbf14194afe78c
nicholsx <nichols.costa@gmail.com>
2021-06-30 17:45:21 -0300

    Verificadas as estruturas das Classes e comentadas
    close #5

--- Atividade8/src/main/java/com/teste/EmpregadoTerceirizado.java
+++ Atividade8/src/main/java/com/teste/EmpregadoTerceirizado.java
@@ -1,5 +1,10 @@
 package com.teste;
 
+/**
+ * 
+ * @author Nicholas - nicholsx
+ * Classe EmpregadoTerceirizado Criada - nicholsx
+ */
 public class EmpregadoTerceirizado {
 	
 	public double adicional;
