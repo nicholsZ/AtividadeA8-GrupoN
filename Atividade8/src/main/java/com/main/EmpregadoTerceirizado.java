b292fd81342f1ee05218932a27229cedd34b1ed2
nicholsx <nichols.costa@gmail.com>
2021-07-02 23:48:02 -0300

    # WARNING: head commit changed in the meantime
    Classe EmpregadoTerceirizado Atualizada
    
    Classe EmpregadoTerceirizadoTest Atualizada

--- Atividade8/src/main/java/com/main/EmpregadoTerceirizado.java
+++ Atividade8/src/main/java/com/main/EmpregadoTerceirizado.java
@@ -2,11 +2,27 @@ package com.main;
 
 /**
  * 
- * @author Nicholas - nicholsx
- * Classe EmpregadoTerceirizado Criada - nicholsx
+ * @author Nicholas - nicholsz
+ * Classe EmpregadoTerceirizado Atualizada com setAdicional - nicholsz
  */
 public class EmpregadoTerceirizado {
 	
 	public double adicional;
 	
+	public double getAdicional() {
+		return adicional;
+	}
+
+	public double setAdicional() {
+		
+		if (adicional < 100) {
+			throw new IllegalArgumentException("Valor minimo R$100,00 / Valor máximo R$1100");
+		}
+		else if (adicional > 1100) {
+			throw new IllegalArgumentException("Valor minimo R$100,00 / Valor máximo R$1100");
+		}
+		else {
+			return adicional;
+		}	
+	}	
 }
--- Atividade8/src/test/java/com/test/EmpregadoTerceirizadoTeste.java
+++ Atividade8/src/test/java/com/test/EmpregadoTerceirizadoTeste.java
@@ -1,14 +1,39 @@
-package com.teste;
+package com.test;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
+import com.main.Empregado;
 
-public class EmpregadoTerceirizadoTest {
+/**
+ * 
+ * @author Nicholas - nicholsz
+ * Classe EmpregadoTerceirizadoTeste Atualizada com testes - nicholsz
+ */
+
+public class EmpregadoTerceirizadoTeste {
 
 	@Test
-	public void testSetAdicional() {
-		fail("Not yet implemented");
+	public void testSetAdicionalIgual() {
+		
+		Empregado funcionarioT = new Empregado("Joao", 30, 30, 500);
+		
+		Assert.assertEquals(500, funcionarioT.setAdicional(), 0.00001);
+	}
+	
+	@Test
+	public void testSetAdicionalMaior() {
+		
+		Empregado funcionarioT = new Empregado("Jose", 30, 30, 2000);
+		
+		Assert.assertEquals(2000, funcionarioT.setAdicional(), 0.00001);
+	}
+	
+	@Test
+	public void testSetAdicionalMenor() {
+		
+		Empregado funcionarioT = new Empregado("Maria", 30, 30, 50);
+		
+		Assert.assertEquals(50, funcionarioT.setAdicional(), 0.00001);
 	}
 
 }
