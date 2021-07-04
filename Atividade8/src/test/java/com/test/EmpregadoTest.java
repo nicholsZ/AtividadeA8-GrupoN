9a0d0e4a9b69be6300f518b4d7e2fd90f2e98a07
nicholsx <nichols.costa@gmail.com>
2021-07-04 15:28:20 -0300

    Classe EmpregadoTest Atualizada - nicholsz
    Issue #15 Resultados dos testes dos metodos.

--- Atividade8/src/test/java/com/test/EmpregadoTest.java
+++ Atividade8/src/test/java/com/test/EmpregadoTest.java
@@ -10,13 +10,18 @@ import com.main.Empregado;
  * 
  * @author Nicholas - nicholsz
  * Classe EmpregadoTest Atualizada - nicholsz
- * Issue #12 Metodos de Testes para setAdicional feitos
+ * Issue #15 Resultado dos testes dos metodos.
  */
 public class EmpregadoTest {
 
 	@Test
 	public void testRealizarPagamentoMaior() {
 		
+		/* testRealizarPagamentoMaior 
+		pagamento = (valorPorHora * horas) + adicional;
+		pagamento = (30 * 40) + 0 = 1200
+		Passou do minimo 1100 e ajustou o teste sendo maior que 1100 dentro dos paramentros
+		*/
 		Empregado funcionario1 = new Empregado("Nicholas", 30, 40, 0);
 			
 		Assert.assertEquals(1200, funcionario1.realizarPagamento(), 0.00001);
@@ -26,7 +31,11 @@ public class EmpregadoTest {
 	@Test
 	public void testRealizarPagamentoMenor() {
 		
-		/* 30 * 30 = 900, com a condiÃ§Ã£o 1100 Ã© o esperado*/
+		/* testRealizarPagamentoMenor 
+		pagamento = (valorPorHora * horas) + adicional;
+		pagamento = (30 * 30) + 0 = 900
+		Não passou do minimo 1100 e ajustou conforme o metodo para 1100.
+		*/
 		Empregado funcionario2 = new Empregado("Maria", 30, 30, 0);
 		
 		Assert.assertEquals(1100, funcionario2.realizarPagamento(), 0.00001);
@@ -35,7 +44,11 @@ public class EmpregadoTest {
 	@Test
 	public void testRealizarPagamentoIgual() {
 		
-		/* (30 * 30) + 200 = 1100 Ã© o esperado*/
+		/* testRealizarPagamentoIgual 
+		pagamento = (valorPorHora * horas) + adicional;
+		pagamento = (30 * 30) + 200 = 1100
+		Igual ao minimo 1100 e ajustou o teste.
+		*/
 		Empregado funcionario3 = new Empregado("Joao", 30, 30, 200);
 		
 		Assert.assertEquals(1100, funcionario3.realizarPagamento(), 0.00001);
@@ -44,6 +57,9 @@ public class EmpregadoTest {
 	@Test
 	public void testSetHorasIgual() {
 		
+		/* testSetHorasIgual 
+		Dentro das restrições de horas 0 até 40
+		*/
 		Empregado funcionarioSH = new Empregado("Joao", 30, 30, 500);
 		
 		int horasSH = 30;
@@ -56,6 +72,9 @@ public class EmpregadoTest {
 	@Test
 	public void testSetHorasMaior() {
 		
+		/* testSetHorasMaior ,atribue a Exception 
+		Acima das restrições de horas 0 até 40
+		*/
 		Empregado funcionarioSH = new Empregado("Roberto", 50, 30, 500);
 		
 		int horasSH = 50;
@@ -68,6 +87,9 @@ public class EmpregadoTest {
 	@Test
 	public void testSetHorasMenor() {
 		
+		/* testSetHorasMenor, atribue a Exception 
+		Abaixo das restrições de horas 0 até 40
+		*/
 		Empregado funcionarioSH = new Empregado("Mario", -10, 30, 500);
 		
 		int horasSH = -10;
@@ -80,6 +102,9 @@ public class EmpregadoTest {
 	@Test
 	public void testSetValorPorHoraIgual() {
 		
+		/* testSetValorPorHoraIgual 
+		Igual as restrições de valorPorHora 30 até 200
+		*/
 		Empregado funcionarioVPH = new Empregado("Claudio", 30, 100, 500);
 		
 		double valorPH = 100;
@@ -92,6 +117,9 @@ public class EmpregadoTest {
 	@Test
 	public void testSetValorPorHoraMaior() {
 		
+		/* testSetValorPorHoraMaior, atribue a Exception 
+		Maior que as restrições de valorPorHora 30 até 200, caindo na Exception.
+		*/
 		Empregado funcionarioVPH = new Empregado("Julio", 30, 300, 500);
 		
 		double valorPH = 300;
@@ -104,6 +132,9 @@ public class EmpregadoTest {
 	@Test
 	public void testSetValorPorHoraMenor() {
 		
+		/* testSetValorPorHoraMenor, atribue a Exception
+		Menor que as restrições de valorPorHora 30 até 200, caindo na Exception.
+		*/
 		Empregado funcionarioVPH = new Empregado("Marieva", 30, 20, 500);
 		
 		double valorPH = 20;
