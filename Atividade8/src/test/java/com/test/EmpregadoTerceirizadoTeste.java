60709f44f06d0f59e45de706ba23d1c35a605ca6
nicholsx <nichols.costa@gmail.com>
2021-07-04 15:34:28 -0300

    Classe EmpregadoTest Atualizada - nicholsz
    Issue #16 Resultado dos testes dos metodos.

--- Atividade8/src/test/java/com/test/EmpregadoTerceirizadoTeste.java
+++ Atividade8/src/test/java/com/test/EmpregadoTerceirizadoTeste.java
@@ -7,14 +7,19 @@ import com.main.Empregado;
 /**
  * 
  * @author Nicholas - nicholsz
- * Classe EmpregadoTerceirizadoTeste atualizada com testes de SetAdicionalVazio - nicholsz
+ * Classe EmpregadoTest Atualizada - nicholsz
+ * Issue #16 Resultado dos testes dos metodos.
  */
 
 public class EmpregadoTerceirizadoTeste {
 
+	
 	@Test
 	public void testSetAdicionalIgual() {
 		
+		/* testSetAdicionalIgual 
+		Dentro das restrições de adicional: 110 até 1100
+		*/
 		Empregado funcionarioT = new Empregado("Joao", 30, 30, 500);
 		
 		Assert.assertEquals(500, funcionarioT.setAdicional(), 0.00001);
@@ -23,6 +28,9 @@ public class EmpregadoTerceirizadoTeste {
 	@Test
 	public void testSetAdicionalMaior() {
 		
+		/* testSetAdicionalMaior 
+		Maior que as restrições de adicional: 110 até 1100
+		*/
 		Empregado funcionarioT = new Empregado("Jose", 30, 30, 2000);
 		
 		Assert.assertEquals(2000, funcionarioT.setAdicional(), 0.00001);
@@ -31,6 +39,9 @@ public class EmpregadoTerceirizadoTeste {
 	@Test
 	public void testSetAdicionalMenor() {
 		
+		/* testSetAdicionalMenor 
+		Menor que as restrições de adicional: 110 até 1100
+		*/
 		Empregado funcionarioT = new Empregado("Maria", 30, 30, 50);
 		
 		Assert.assertEquals(50, funcionarioT.setAdicional(), 0.00001);
@@ -40,6 +51,9 @@ public class EmpregadoTerceirizadoTeste {
 	@Test
 	public void testSetAdicionalVazioIgual() {
 		
+		/* testSetAdicionalVazioIgual 
+		Dentro das restrições de adicional: 110 até 1100
+		*/
 		Empregado funcionarioAV = new Empregado("Marieva", 30, 30, 500);
 		
 		double adicionalV = 500;
@@ -52,6 +66,9 @@ public class EmpregadoTerceirizadoTeste {
 	@Test
 	public void testSetAdicionalVazioMaior() {
 		
+		/* testSetAdicionalVazioMaior 
+		Dentro das restrições de adicional: 110 até 1100
+		*/
 		Empregado funcionarioAV = new Empregado("Julieta", 30, 30, 1500);
 		
 		double adicionalV = 1500;
@@ -64,6 +81,9 @@ public class EmpregadoTerceirizadoTeste {
 	@Test
 	public void testSetAdicionalVazioMenor() {
 		
+		/* testSetAdicionalVazioMenor 
+		Dentro das restrições de adicional: 110 até 1100
+		*/
 		Empregado funcionarioAV = new Empregado("Katia", 30, 30, 50);
 		
 		double adicionalV = 50;
